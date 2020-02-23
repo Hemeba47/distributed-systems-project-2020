95e580b93e9e29f659b135b303031693d3366a0d
tvaisanen <vaisanen.toni@gmail.com>
2020-02-23 16:30:17 +0200

    Add configs to logging

--- services/auth/src/index.ts
+++ services/auth/src/index.ts
@@ -1,62 +1,62 @@
-import "reflect-metadata";
-import { createConnection, getRepository } from "typeorm";
-import * as express from "express";
-import * as bodyParser from "body-parser";
-import { Request, Response } from "express";
-import { Routes } from "./routes";
-import { User } from "./entity/User";
-import * as bcrypt from "bcryptjs";
-import * as amqp from "amqplib";
-import axios from "axios";
+import 'reflect-metadata'
+import { createConnection, getRepository } from 'typeorm'
+import * as express from 'express'
+import * as bodyParser from 'body-parser'
+import { Request, Response } from 'express'
+import { Routes } from './routes'
+import { User } from './entity/User'
+import * as bcrypt from 'bcryptjs'
+import * as amqp from 'amqplib'
+import axios from 'axios'
 
-const exphbs = require("express-handlebars");
+const exphbs = require('express-handlebars')
 
 createConnection()
   .then(async connection => {
     /* RABBIT MQ MESSAGE PUBLISH */
-    const rabbitmqConnection = await amqp.connect("amqp://rabbitmq");
+    const rabbitmqConnection = await amqp.connect('amqp://rabbitmq')
 
-    const channel = await rabbitmqConnection.createChannel();
-    var queue = "logging";
-    var msg = "logginservice started now";
+    const channel = await rabbitmqConnection.createChannel()
+    var queue = 'logging'
+    var msg = 'Auth Service Started'
 
     channel.assertQueue(queue, {
-      durable: false
-    });
+      durable: false,
+    })
 
-    channel.sendToQueue(queue, Buffer.from(msg));
+    channel.sendToQueue(queue, Buffer.from(msg))
 
     // create express app
-    const app = express();
-    app.engine("handlebars", exphbs());
-    app.set("view engine", "handlebars");
-    app.use(bodyParser.json());
-    app.use(bodyParser.urlencoded({ extended: true }));
+    const app = express()
+    app.engine('handlebars', exphbs())
+    app.set('view engine', 'handlebars')
+    app.use(bodyParser.json())
+    app.use(bodyParser.urlencoded({ extended: true }))
 
-    app.get("/", (req: Request, response: Response, next: Function) => {
-      return response.render("login.hbs");
-    });
+    app.get('/', (req: Request, response: Response, next: Function) => {
+      return response.render('login.hbs')
+    })
 
-    app.get("/login", (req: Request, res: Response, next: Function) => {
-      return res.render("login.hbs");
-    });
+    app.get('/login', (req: Request, res: Response, next: Function) => {
+      return res.render('login.hbs')
+    })
 
-    app.post("/login", async (req: Request, res: Response, next: Function) => {
+    app.post('/login', async (req: Request, res: Response, next: Function) => {
       const user = await getRepository(User).findOne({
-        email: req.body.email
-      });
+        email: req.body.email,
+      })
 
       if (user) {
         /**
          * Log user in if the password hashes match
          */
-        const pwOk = bcrypt.compareSync(req.body.password, user.password);
+        const pwOk = bcrypt.compareSync(req.body.password, user.password)
 
         if (!pwOk) {
           /**
            * If not just return 401
            */
-          return res.sendStatus(401);
+          return res.sendStatus(401)
         }
 
         /**
@@ -64,124 +64,124 @@ createConnection()
          * user info for the service that the client
          * is being redirected to
          */
-        const uriEncodedUser = encodeURIComponent(JSON.stringify(user));
+        const uriEncodedUser = encodeURIComponent(JSON.stringify(user))
 
-        if (req.body.service === "chat") {
+        if (req.body.service === 'chat') {
           return res.redirect(
-            `http://localhost:5000/login-success?user=${uriEncodedUser}`
-          );
+            `http://localhost:5000/login-success?user=${uriEncodedUser}`,
+          )
         }
-        if (req.body.service === "profile") {
+        if (req.body.service === 'profile') {
           return res.redirect(
-            `http://localhost:5001/profile?user=${uriEncodedUser}`
-          );
+            `http://localhost:5001/profile?user=${uriEncodedUser}`,
+          )
         }
       }
 
-      return res.json(user);
-    });
+      return res.json(user)
+    })
 
-    app.get("/register", (req: Request, res: Response, next: Function) => {
-      return res.render("register.hbs");
-    });
+    app.get('/register', (req: Request, res: Response, next: Function) => {
+      return res.render('register.hbs')
+    })
 
     app.post(
-      "/register",
+      '/register',
       async (req: Request, res: Response, next: Function) => {
         try {
           const user = new User({
             ...req.body,
-            password: bcrypt.hashSync(req.body.password, 8)
-          });
+            password: bcrypt.hashSync(req.body.password, 8),
+          })
           const { password, ...savedUser } = await getRepository(User).save(
-            user
-          );
-          console.log(savedUser);
+            user,
+          )
+          console.log(savedUser)
           const redirectUrl = `/profile?user=${encodeURIComponent(
-            JSON.stringify(savedUser)
-          )}`;
-          console.log(redirectUrl);
-          return res.redirect(redirectUrl);
+            JSON.stringify(savedUser),
+          )}`
+          console.log(redirectUrl)
+          return res.redirect(redirectUrl)
         } catch (err) {
-          console.log(err);
-          return res.sendStatus(400);
+          console.log(err)
+          return res.sendStatus(400)
         }
-      }
-    );
+      },
+    )
 
     /**
      * Render the logged in users information
      */
-    app.get("/profile", (req: Request, res: Response, next: Function) => {
-      const loggedInUser = JSON.parse(req.query.user);
+    app.get('/profile', (req: Request, res: Response, next: Function) => {
+      const loggedInUser = JSON.parse(req.query.user)
       //(req as any).session.user = loggedInUser;
-      return res.render("profile.hbs", {
+      return res.render('profile.hbs', {
         userURIComponent: encodeURIComponent(JSON.stringify(loggedInUser)),
-        ...loggedInUser
-      });
-    });
+        ...loggedInUser,
+      })
+    })
 
     /**
      * Run a test to see how long it takes to
      * send N amount of requests between nodes
      */
-    app.post("/run-tests", async (req, res) => {
+    app.post('/run-tests', async (req, res) => {
       // if requestCount provided with the initiating
       // request use that, if not use 30 as default
-      const reqCount = Number(req.query.requestCount) || 30;
+      const reqCount = Number(req.query.requestCount) || 30
       // create range [0...reqCount]
-      const range = [...Array(reqCount).keys()];
+      const range = [...Array(reqCount).keys()]
       // if testName provided with the initiating
       // request use that, if not use "default-test" as default
-      const testName = req.query.testName || "default-test";
+      const testName = req.query.testName || 'default-test'
       // keep track how long it took to send all N requests
-      const t0 = new Date();
+      const t0 = new Date()
       const d = await Promise.all(
         range.map(i => {
           return axios.get(
-            `http://chat:3000/test-receiver?test=${testName}&requestNumber=${i}`
-          );
-        })
-      );
+            `http://chat:3000/test-receiver?test=${testName}&requestNumber=${i}`,
+          )
+        }),
+      )
       // time after the requests were sent
-      const t1 = new Date();
+      const t1 = new Date()
       // return info about the test to the client
       return res.json({
         msg: `ran ${reqCount} requests in time t0 - t1`,
         t0,
-        t1
-      });
-    });
+        t1,
+      })
+    })
 
     // register express routes from defined application routes
     Routes.forEach(route => {
-      (app as any)[route.method](
+      ;(app as any)[route.method](
         route.route,
         (req: Request, res: Response, next: Function) => {
           const result = new (route.controller as any)()[route.action](
             req,
             res,
-            next
-          );
+            next,
+          )
           if (result instanceof Promise) {
             result.then(result =>
               result !== null && result !== undefined
                 ? res.send(result)
-                : undefined
-            );
+                : undefined,
+            )
           } else if (result !== null && result !== undefined) {
-            res.json(result);
+            res.json(result)
           }
-        }
-      );
-    });
+        },
+      )
+    })
 
-    app.listen(3000);
+    app.listen(3000)
 
-    console.log("Auth Server Running...");
+    console.log('Auth Server Running...')
   })
 
   .catch(error => {
-    console.log(error.message);
-    console.log(error);
-  });
+    console.log(error.message)
+    console.log(error)
+  })
--- services/logging/src/index.js
+++ services/logging/src/index.js
@@ -1,45 +1,58 @@
 //import createConnection from "typeorm";
 
-var express = require("express");
-var app = express();
-var amqp = require('amqplib/callback_api');
-var fs = require('fs');
+var express = require('express')
+var app = express()
+var amqp = require('amqplib/callback_api')
+var fs = require('fs')
+const path = require('path')
 
-//page localhost:5002/
-//app.get("/", function(req, res) {
-//  res.send("Hello W0rld!");
-//});
+const logPath = process.env.LOGPATH || ''
+const logFile = process.env.LOGFILE || 'message-log.txt'
+
+const fullLogFilePath = path.join(logPath, logFile)
+
+fs.appendFileSync(fullLogFilePath, `[START] ${new Date().toISOString()}\n`)
 
 amqp.connect('amqp://rabbitmq', function(error0, connection) {
   if (error0) {
-    throw error0;
+    throw error0
   }
   connection.createChannel(function(error1, channel) {
     if (error1) {
-      throw error1;
+      throw error1
     }
-    var queue = 'logging';
-    var msg = "Logging service started now";
-
+    var queue = 'logging'
+    var msg = 'Logging service started now'
     channel.assertQueue(queue, {
-      durable: false
-    });
+      durable: false,
+    })
 
-    console.log("Logger started logging logs to log them in the log.");
+    console.log('Logger started logging logs to log them in the log.')
 
-    channel.consume(queue, (msg) => {
-      console.log("Received: " + msg.content);
-      // this appendFile doesn't work
-      fs.appendFile('message-log.txt', msg.content, (error2) => {
-        if (error2) throw error2;
-        console.log('The file was appended!');
-      });
-      //This appendFileSync neither
-      fs.appendFileSync("message-log.txt", msg.content);
-    });
-  });
-});
+    channel.consume(queue, msg => {
+      console.log('Received: ' + msg.content)
+
+      /**
+       * This works fine, it's just writing the
+       * file inside the docker container.
+       *
+       * I added a variable for the filename
+       * so it's easier to configure and not
+       * to typo
+       *
+       * also added the new line in the beginning
+       * of a log message with a timestamp
+       */
+      fs.appendFileSync(
+        fullLogFilePath,
+        `\n[${msg.fields.routingKey}] ${new Date().toISOString()} ${
+          msg.content
+        }`,
+      )
+    })
+  })
+})
 
 app.listen(3000, function() {
-  console.log("Example app listening on port 3000!");
-});
+  console.log('Example app listening on port 3000!')
+})
